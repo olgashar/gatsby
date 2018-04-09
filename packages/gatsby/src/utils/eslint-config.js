62f5e0e9542f5d96b7e1e3db7a281c4ab52caf64
Kurtis Kemple <kurtiskemple@gmail.com>
2018-04-08 21:28:47 -0400

    feat: add eslint loader to webpack config
    Signed-off-by: Kurtis Kemple <kurtiskemple@gmail.com>

---
+++ packages/gatsby/src/utils/eslint-config.js
@@ -0,0 +1,31 @@
+import { printSchema } from "graphql"
+
+module.exports = schema => {
+  return {
+    useEslintrc: false,
+    baseConfig: {
+      globals: {
+        graphql: true,
+      },
+      extends: `react-app`,
+      plugins: [`graphql`],
+      rules: {
+        "graphql/template-strings": [
+          `error`,
+          {
+            env: `relay`,
+            schemaString: printSchema(schema),
+            tagName: `graphql`,
+          },
+        ],
+        "graphql/named-operations": [
+          `warn`,
+          {
+            env: `relay`,
+            schemaString: printSchema(schema),
+          },
+        ],
+      },
+    },
+  }
+}
